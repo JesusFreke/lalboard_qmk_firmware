f2f1e83b720a883c01508b70cb3675e32fa2a36f
Ben Gruver <jesusfreke@jesusfreke.com>
2023-11-17 14:45:19 -0600

    Initial lalboard keyboard implementation

---
+++ keyboards/lalboard/config.h
@@ -0,0 +1,42 @@
+/* Copyright 2023 Ben Gruver <jesusfreke@jesusfreke.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 5
+
+// The column with the "down" keys on both the normal and thumb clusters
+// is inverted.
+#define MATRIX_COL_PUSHED_STATES { 0, 0, 1, 0, 0 }
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 0
+
+#define EE_HANDS
+
+//#define DEBUG_MATRIX_SCAN_RATE
+
+#define SERIAL_DEBUG
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP24
+#define SERIAL_USART_RX_PIN GP25
+#define SERIAL_USART_PIN_SWAP
---
+++ keyboards/lalboard/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = RP2040
+BOOTLOADER = rp2040
+
+SERIAL_DRIVER = vendor
+
+CUSTOM_MATRIX = lite
+
+# Project specific files
+SRC = matrix.c
